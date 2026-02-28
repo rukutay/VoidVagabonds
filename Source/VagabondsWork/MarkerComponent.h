f523df2a9483ead5566fa135fe5e7317c8dcec38
rukutay <ivanchenko22@gmail.com>
2026-02-28 12:47:33 +0100

    Update marker factions and display names

--- Source/VagabondsWork/MarkerComponent.h
+++ Source/VagabondsWork/MarkerComponent.h
@@ -7,18 +7,21 @@
 UENUM(BlueprintType)
 enum class EMarkerType : uint8
 {
-	Ship,
-	Star,
-	Planet,
-	Component
+	Ship UMETA(DisplayName = "Ship"),
+	Star UMETA(DisplayName = "Star"),
+	Planet UMETA(DisplayName = "Planet"),
+	Component UMETA(DisplayName = "Component")
 };
 
 UENUM(BlueprintType)
 enum class EFaction : uint8
 {
-	Neutral,
-	Red,
-	Blue
+	VoidVagabonds UMETA(DisplayName = "Void Vagabonds"),
+	VoidRaiders UMETA(DisplayName = "Void Raiders"),
+	SilentMandate UMETA(DisplayName = "Silent Mandate"),
+	FederationOfNations UMETA(DisplayName = "Federation of Nations"),
+	UnitedRepublic UMETA(DisplayName = "United Republic"),
+	ConcordUnion UMETA(DisplayName = "Concord Union")
 };
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
@@ -32,5 +35,5 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Marker")
 	EMarkerType MarkerType = EMarkerType::Component;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Faction")
-	EFaction Faction = EFaction::Neutral;
+	EFaction Faction = EFaction::VoidVagabonds;
 };
